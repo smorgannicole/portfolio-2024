224ecf1b7ff0f2d6ff5b9da08eb1c4f8a53ab9fb
smorgannicole <morgann.smithh@yahoo.com>
2024-04-26 07:31:40 -0700

    deployment issues

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState } from "react"
+import React, { useRef } from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faTimesCircle } from "@fortawesome/free-regular-svg-icons"
 import Carousel from "./Carousel.js"
